17bf45e66bc7858c7bc81f4fc1b29b0986570233
amir <amirgreat2000@gmail.com>
2020-04-02 11:36:43 +0200

    Update tag line text

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'tagline' => 'Personal audio streaming service that works.',
+    'tagline' => 'Daawa salafia in Habesha audio streaming service.',
 
     'env' => env('APP_ENV', 'production'),
 
